af453caf42550c5bd8365857c0b6820db1e9523d
jim-clark <email@jim-clark.com>
2019-10-24 13:16:36 -0700

    Update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*----- constants -----*/
+const LOSE_WRONG_COUNT = 6;
+
+/*----- app's state (variables) -----*/
+let secretWord, guessWord, lettersUsed, wrongLetters;
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/

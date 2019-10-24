be53e7ae264a9e5682140967217fa78f5c57e95d
jim-clark <email@jim-clark.com>
2019-10-24 13:58:57 -0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
 /*----- constants -----*/
 const LOSE_WRONG_COUNT = 6;
+const WORDS = [
+  'DEVELOPER', 'HTML', 'JAVASCRIPT',
+  'PROGRAM', 'CODE', 'FUNCTION',
+  'RECURSION', 'REACT'
+];
 
 /*----- app's state (variables) -----*/
 let secretWord, guessWord, lettersUsed, wrongLetters;
 
 /*----- cached element references -----*/
-
+const guessWordEl = document.getElementById('word');
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+init();
+
+function render() {
+  // display message
+
+  // display gallows
+
+  // display guessWord
+  guessWordEl.textContent = guessWord;
+  // update letters
+
+}
+
+function init() {
+  let rndIdx = Math.floor(Math.random() * WORDS.length);
+  secretWord = WORDS[rndIdx];
+  guessWord = '_'.repeat(secretWord.length);
+  lettersUsed = [];
+  wrongLetters = [];
+  render();
+}
